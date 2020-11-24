c5f325b1ff7ecb9150bcb639525df2eb36ae9993
Uwe Jettkant <uj@MacBook-Pro-6.betahaushh.lan>
2020-11-24 14:23:17 +0100

    added linkedin icon and link
    Signed-off-by: Uwe Jettkant <uj@MacBook-Pro-6.betahaushh.lan>

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57668" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64968" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
